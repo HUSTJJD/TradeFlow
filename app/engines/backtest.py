4364ba4b55776c414f282a424d864524a706d7db
HUSTJJD <1173703591@qq.com>
2026-01-17 22:35:12 +0800

    重构

--- app/engines/backtest.py
+++ app/engines/backtest.py
@@ -1,16 +1,10 @@
 import logging
 import pandas as pd
-from datetime import date, datetime, timedelta
 from typing import Dict, List, Any, Optional, cast
-from longport.openapi import QuoteContext, Period
-from sqlalchemy import over
-
 from app.core import cfg
 from app.strategies import Strategy
 from app.engines.engine import Engine
-from app.providers.longport import LongPortProvider
-from app.utils.reporting import print_backtest_summary
-from app.utils.plotter import create_performance_chart
+
 
 logger = logging.getLogger(__name__)
 
@@ -18,13 +12,9 @@ logger = logging.getLogger(__name__)
 class BacktestEngine(Engine):
     """回测执行引擎"""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.data: Dict[str, pd.DataFrame] = {}
+    def __init__(self):
+        super().__init__()
 
-    def run(self) -> Dict[str, Any]:
+    def run(self):
         """运行回测"""
-        if not self.data:
-            logger.error("回测数据为空")
-            return {}
-        
+        logger.error("回测数据为空")
--- app/engines/paper.py
+++ app/engines/paper.py
@@ -2,4 +2,11 @@ from app.engines.engine import Engine
 
 
 class PaperEngine(Engine):
-    pass
+    """模拟盘执行引擎"""
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        """运行模拟盘监控"""
+        pass
