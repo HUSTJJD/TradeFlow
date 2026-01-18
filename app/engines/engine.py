8bca7c0564fb0bdf567735e2046d8d8c162a1783
HUSTJJD <1173703591@qq.com>
2026-01-18 21:16:06 +0800

    重构

--- app/engines/engine.py
+++ app/engines/engine.py
@@ -6,9 +6,9 @@ import logging
 from longport.openapi import QuoteContext, Period
 from app.core import cfg, ActionType, TradeMode
 from app.strategies import Strategy
-from app.trading import create_account
+from app.trading import Account
 from app.providers import create_provider
-from app.utils import update_market_symbols, update_stock_datas
+from app.utils import Dataset
 
 logger = logging.getLogger(__name__)
 
@@ -19,12 +19,13 @@ class Engine(ABC):
     def __init__(self):
         # 初始化数据提供器
         self.provider = create_provider()
-        self.account = create_account()
+        self.account = Account()
+        self.dataset = Dataset()
 
     def run(self):
         """运行策略执行引擎"""
         self.update_market_data()
     
     def update_market_data(self) -> None:
-        update_market_symbols()
-        update_stock_datas()
+        self.dataset.update_market_symbols()
+        self.dataset.update_stock_datas()
--- app/utils/dataset.py
+++ app/utils/dataset.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import logging
 from datetime import datetime, timedelta, date
 import pandas as pd
@@ -9,11 +10,13 @@ logger = logging.getLogger(__name__)
 
 class Dataset:
     """数据集工具类，提供市场标的和历史数据的获取与更新功能。"""
-    SYMBOL_FILE_PATH = "data/market_symbols.csv"
-    DATA_FILE_PATH = "data/stocks/{symbol}.parquet"
+    SYMBOL_FILE_PATH = Path("data/market_symbols.csv")
+    DATA_FILE_PATH = Path("data/stocks/")
     
     def __init__(self) -> None:
-        self.market_symbols = self.update_market_symbols()
+        self.SYMBOL_FILE_PATH.parent.mkdir(parents=True, exist_ok=True)
+        self.DATA_FILE_PATH.mkdir(parents=True, exist_ok=True)
+        self.update_market_symbols()
         self.stock_datas = pd.DataFrame()
 
     def update_stock_datas(self, symbols: list[str] = []) -> None:
@@ -21,63 +24,67 @@ class Dataset:
             symbols = self.market_symbols['symbol'].tolist()
         for symbol in symbols:
             # 增量更新
-            df = pd.read_parquet(self.DATA_FILE_PATH.format(symbol=symbol))
-            df = df[df['date'] > (date.today() - timedelta(days=365*10)).strftime("%Y%m%d")]
-            df = df[df['date'] < date.today().strftime("%Y%m%d")]
-            df = df.drop_duplicates(subset=["date"])
-            new_df = self.fetch_stock_data(symbol, date.today() - timedelta(days=365*10), date.today())
-            df = pd.concat([df, new_df], ignore_index=True).drop_duplicates(subset=["date"])
-            df.to_parquet(self.DATA_FILE_PATH.format(symbol=symbol))
+            file_path = self.DATA_FILE_PATH.joinpath(f"{symbol}.parquet")
+            stock = self.market_symbols.get(symbol)
+            if stock is None:
+                continue
+            if stock["date"] is not pd.NaT:
+                start_date = stock["date"]
+            else:
+                start_date = date.today() - timedelta(days=365*10)
+            end_date = date.today()
+            if os.path.exists(file_path):
+                df = pd.read_parquet(file_path)
+            else:
+                df = pd.DataFrame()
+            try:
+                new_df = self.fetch_stock_data(stock["origin_symbol"], start_date, end_date)
+                df = pd.concat([df, new_df], ignore_index=True)
+                df.to_parquet(file_path)
+            except Exception as e:
+                logger.error(f"更新 {symbol} 历史数据失败: {e}")
 
     def fetch_stock_data(self, symbol: str, start_date: date, end_date: date) -> pd.DataFrame:
         """获取单只股票的历史数据"""
         logger.info(f"开始获取 {symbol} 的历史数据...")
         return ak.stock_zh_a_hist(symbol=symbol, period="daily", start_date=start_date.strftime("%Y%m%d"), end_date=end_date.strftime("%Y%m%d"))
 
-    def update_market_symbols(self) -> pd.DataFrame:
+    def update_market_symbols(self):
         """
         获取全市场标的（A股 + 港股通），支持本地缓存和定期更新。
         """
         file_path = self.SYMBOL_FILE_PATH
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
         if os.path.exists(file_path):
             file_mtime = datetime.fromtimestamp(os.path.getmtime(file_path))
             if datetime.now() - file_mtime < timedelta(days=30):
-                return pd.read_csv(file_path)
-        logger.info("开始更新市场标的数据...")
-        df = self.fetch_market_symbols()
-        df.to_csv(file_path, index=False)
-        logger.info(f"市场标的数据已更新并保存至: {file_path}, 共 {len(df)} 条")
-        return df
-
-
-    def fetch_market_symbols(self) -> pd.DataFrame:
-        """
-        从 AkShare 获取数据并合并
-        """
-        stock_sh = ak.stock_info_sh_name_code(symbol="主板A股")
-        stock_sh = stock_sh[["证券代码", "证券简称", "上市日期"]]
-        stock_sh = stock_sh.rename(columns={"证券代码": "origin_symbol", "证券简称": "name", "上市日期": "date"})
-        stock_kcb = ak.stock_info_sh_name_code(symbol="科创板")
-        stock_kcb = stock_kcb[["证券代码", "证券简称", "上市日期"]]
-        stock_kcb = stock_kcb.rename(columns={"证券代码": "origin_symbol", "证券简称": "name", "上市日期": "date"})
-        stock_sz = ak.stock_info_sz_name_code(symbol="A股列表")
-        stock_sz = stock_sz[["A股代码", "A股简称", "A股上市日期", "所属行业"]]
-        stock_sz = stock_sz.rename(columns={"A股代码": "origin_symbol", "A股简称": "name", "A股上市日期": "date", "所属行业": "industry"})
-        stock_bse = ak.stock_info_bj_name_code()
-        stock_bse = stock_bse[["证券代码", "证券简称", "上市日期", "所属行业"]]
-        stock_bse = stock_bse.rename(columns={"证券代码": "origin_symbol", "证券简称": "name", "上市日期": "date", "所属行业": "industry"})
-        df_all = pd.concat([stock_sh, stock_kcb, stock_sz, stock_bse], ignore_index=True)
-        df_all[["symbol", "board"]] = df_all["origin_symbol"].apply(
-            lambda x: pd.Series(self.get_stock_symbol_and_board(x))
-        )
-        stock_hk = ak.stock_hk_ggt_components_em()
-        stock_hk = stock_hk[["代码", "名称"]]
-        stock_hk = stock_hk.rename(columns={"代码": "origin_symbol", "名称": "name"})
-        stock_hk["symbol"] = stock_hk["origin_symbol"].apply(lambda x: f"{x}.HK")
-        stock_hk["board"] = MarketType.HK.value
-        df_all = pd.concat([df_all, stock_hk], ignore_index=True)
-        return df_all
+                self.market_symbols = pd.read_csv(file_path)
+        else:
+            logger.info("开始更新市场标的数据...")
+            stock_sh = ak.stock_info_sh_name_code(symbol="主板A股")
+            stock_sh = stock_sh[["证券代码", "证券简称", "上市日期"]]
+            stock_sh = stock_sh.rename(columns={"证券代码": "origin_symbol", "证券简称": "name", "上市日期": "date"})
+            stock_kcb = ak.stock_info_sh_name_code(symbol="科创板")
+            stock_kcb = stock_kcb[["证券代码", "证券简称", "上市日期"]]
+            stock_kcb = stock_kcb.rename(columns={"证券代码": "origin_symbol", "证券简称": "name", "上市日期": "date"})
+            stock_sz = ak.stock_info_sz_name_code(symbol="A股列表")
+            stock_sz = stock_sz[["A股代码", "A股简称", "A股上市日期", "所属行业"]]
+            stock_sz = stock_sz.rename(columns={"A股代码": "origin_symbol", "A股简称": "name", "A股上市日期": "date", "所属行业": "industry"})
+            stock_bse = ak.stock_info_bj_name_code()
+            stock_bse = stock_bse[["证券代码", "证券简称", "上市日期", "所属行业"]]
+            stock_bse = stock_bse.rename(columns={"证券代码": "origin_symbol", "证券简称": "name", "上市日期": "date", "所属行业": "industry"})
+            df_all = pd.concat([stock_sh, stock_kcb, stock_sz, stock_bse], ignore_index=True)
+            df_all[["symbol", "board"]] = df_all["origin_symbol"].apply(
+                lambda x: pd.Series(self.get_stock_symbol_and_board(x))
+            )
+            stock_hk = ak.stock_hk_ggt_components_em()
+            stock_hk = stock_hk[["代码", "名称"]]
+            stock_hk = stock_hk.rename(columns={"代码": "origin_symbol", "名称": "name"})
+            stock_hk["symbol"] = stock_hk["origin_symbol"].apply(lambda x: f"{x}.HK")
+            stock_hk["board"] = MarketType.HK.value
+            df_all = pd.concat([df_all, stock_hk])
+            df_all.set_index(df_all["symbol"], inplace=False)
+            df_all.to_csv(file_path, index=True)
+            self.market_symbols = df_all
 
 
     def get_stock_symbol_and_board(self, code: str) -> tuple[str, str]:
